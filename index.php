88b5380b975c36310a15eba33c22d6101295fdad
Kyle Surowiec <kylesurowiec@me.com>
2018-06-18 13:57:20 -0500

    formatting

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <html>
-<head>
-  <meta charset="UTF-8" />
-</head>
+  <head>
+    <meta charset="UTF-8" />
+  </head>
 
-<?php
-  if (isset($_POST['LightON']))
-  {
-    echo "ON";
-    exec("sudo python /home/pi/lightOn.py");
+  <?php
+    
+    if (isset($_POST['LightON'])) {
+      echo "ON";
+      exec("sudo python /home/pi/lightOn.py");
     }
-    if (isset($_POST['LightOFF']))
-    {
-    echo "OFF";
-    exec("sudo python /home/pi/lightOff.py");
-  }
-?>
+     
+    if (isset($_POST['LightOFF'])) {
+      echo "OFF";
+      exec("sudo python /home/pi/lightOff.py");
+    }
+  
+  ?>
 
-<form method="post">
-  <button class="btn" name="LightON">On</button>&nbsp;
-  <button class="btn" name="LightOFF">Off</button><br><br>
-</form> 
+  <form method="post">
+    <button class="btn" name="LightON">On</button>&nbsp;
+    <button class="btn" name="LightOFF">Off</button><br><br>
+  </form> 
 
 </html>
